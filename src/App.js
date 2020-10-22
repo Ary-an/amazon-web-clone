b72677faad62f4ba8ddbfe529e622371cbe84398
unknown <mdaryan313@gmail.com>
2020-10-22 13:42:23 +0530

    build amazon web clone

--- src/App.js
+++ src/App.js
@@ -1,55 +1,70 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { auth } from "./firebase";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
+import { auth } from "./firebase";
 import Header from "./components/Header";
 import { useStateValue } from "./context/StateProvide";
 import Home from "./components/Home";
 import Checkout from "./components/Checkout";
 import Login from "./components/Login";
+import Orders from "./components/Orders";
+import Payment from "./components/Payment";
+require("dotenv").config();
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_PAYMENT_KEY);
 
 function App() {
-	const [{}, dispatch] = useStateValue();
+  const [{}, dispatch] = useStateValue();
 
-	useEffect(() => {
-		auth.onAuthStateChanged((authUser) => {
-			console.log("THE USER IS >>> ", authUser);
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
 
-			if (authUser) {
-				dispatch({
-					type: "SET_USER",
-					user: authUser,
-				});
-			} else {
-				dispatch({
-					type: "SET_USER",
-					user: null,
-				});
-			}
-		});
-	}, []);
-
-	return (
-		<Router>
-			<div className="app">
-				<Switch>
-					<Route path="/login">
-						<Header />
-						<Login />
-					</Route>
-					<Route path="/checkout">
-						<Header />
-						<Checkout />
-					</Route>
-					<Route path="/">
-						<Header />
-						<Home />
-					</Route>
-				</Switch>
-			</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+          <Route path="/login">
+            <Header />
+            <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
